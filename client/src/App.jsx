c5a891d333019f6b9c1acc92c66aa37bdbccb14b
Samarth <moresamarth504@gmail.com>
2025-07-04 17:32:04 +0530

    Switched to plain CSS from Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Document Signature App</h1>
+      <p>Welcome to the app!</p>
+    </div>
+  );
+}
+
+export default App;
